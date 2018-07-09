ced8721d78bdc4a661a38e6db2f7986c5ed95d62
Filipe-AS <filipealvesdossantos@hotmail.com>
2018-07-09 16:53:17 -0300

    springsecurity-teste

---
+++ src/main/java/br/com/filipe/angular/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.filipe.angular.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll().anyRequest()
+				.authenticated().and().formLogin().permitAll().and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        @SuppressWarnings("deprecation")
+		UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("filipe")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
